f6eeb0610cfc3505d518e1889573948028fb609c
Michael <mkeating2225@gmail.com>
2016-10-21 17:21:24 -0400

    renaming theme

--- template-parts/content-landing.php
+++ template-parts/content-landing.php
@@ -36,26 +36,31 @@
 				jQuery(function($) {
 					var ID = <?php echo json_encode($post->post_name);?> ;
 					var background = <?php echo json_encode(get_post_meta($post->ID, "_background", true));?> ;
-					var background_image_wide = <?php echo json_encode(get_post_meta($post->ID, "_background_img_wide", true));?> ;
+					var background_image = <?php echo json_encode(get_post_meta($post->ID, "_background_img", true));?> ;
+					/*var background_image_wide = <?php echo json_encode(get_post_meta($post->ID, "_background_img_wide", true));?> ;
 					var background_image_portrait = <?php echo json_encode(get_post_meta($post->ID, "_background_img_portrait", true));?> ;
+
+					console.log("testing bg wide:" + background_image_wide);*/
+
 
 					if(background){
 						$("#"+ID).css('background-color', background); 
 					}
-					if(background_image_wide){
-						$("#"+ID).css('background', 'url('+ background_image_wide + ') no-repeat center center '); 
+					if(background_image){
+						$("#"+ID).css('background', 'url('+ background_image + ') no-repeat center center '); 
 						$("#"+ID).css('-webkit-background-size', 'cover');
 						$("#"+ID).css('-moz-background-size:', 'cover');
 						$("#"+ID).css('-o-background-size', 'cover');
 						$("#"+ID).css('background-size', 'cover');
 					}
+					/*
 					if (background_image_wide && background_image_portrait) {
 						$("#"+ID).css('background', 'url('+ background_image_portrait + ') no-repeat center center '); 
 						$("#"+ID).css('-webkit-background-size', 'cover');
 						$("#"+ID).css('-moz-background-size:', 'cover');
 						$("#"+ID).css('-o-background-size', 'cover');
 						$("#"+ID).css('background-size', 'cover');
-					}
+					}*/
 
 					var next_target = <?php echo json_encode(get_post_meta($post->ID, "_nxt_btn_target", true));?> ;
 					var next_text = <?php echo json_encode(get_post_meta($post->ID, "_nxt_btn_text", true));?> ;
